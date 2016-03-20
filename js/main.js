b6a6091467f4c5fff24c72aa101bf01d35404f64
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-03-20 01:06:54 +0100

    Menu and basic sections started
    The boilerplate was divided into major sections. Basic color style
    guide  implemented.
    Menu still needs more styling. The page is ready for preparing content
    sections.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 // The main JS for Portfolio Free Code Camp project
+// Mobile menu collapse
+$(document).on('click','.navbar-collapse a',function(e) {
+    $('.navbar-collapse').collapse('hide');
+});
