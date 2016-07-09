6877aa6440c84100efa7c8a0738e7569715e1703
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2016-07-09 20:09:46 +0200

    add tictactoe game to portfolio and change http to https in links where possible

--- js/main.js
+++ js/main.js
@@ -15,23 +15,26 @@ $( "#thedoer" ).click(function() {
     window.location.href="http://theonewhodo.es/doerinmalaga/";
 });
 $( "#tribute" ).click(function() {
-    window.location.href="http://s.codepen.io/thedoer/debug/pybKVp";
+    window.location.href="https://s.codepen.io/thedoer/debug/pybKVp";
 });
 $( "#portfolio-fcc" ).click(function() {
     window.location.href="#about";
 });
 $( "#quote-machine" ).click(function() {
-    window.location.href="http://s.codepen.io/thedoer/debug/rezOLx";
+    window.location.href="https://s.codepen.io/thedoer/debug/rezOLx";
 });
 $( "#sheep-weather-app" ).click(function() {
-    window.location.href="http://s.codepen.io/thedoer/debug/vGeYZp";
+    window.location.href="https://s.codepen.io/thedoer/debug/vGeYZp";
 });
 $( "#wiki-viewer" ).click(function() {
-    window.location.href="http://s.codepen.io/thedoer/debug/EKbrpO";
+    window.location.href="https://s.codepen.io/thedoer/debug/EKbrpO";
 });
 $( "#game-watcher" ).click(function() {
-    window.location.href="http://s.codepen.io/thedoer/debug/GZxdQO";
+    window.location.href="https://s.codepen.io/thedoer/debug/GZxdQO";
 });
 $( "#js-calculator" ).click(function() {
-    window.location.href="http://s.codepen.io/thedoer/debug/RaEOme";
+    window.location.href="https://s.codepen.io/thedoer/debug/RaEOme";
+});
+$( "#tictactoe" ).click(function() {
+    window.location.href="http://theonewhodo.es/tictactoe/";
 });
